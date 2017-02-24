16dfe3b6ed549cd481714dbf236834349db4a984
Jeffrey Elkner <jeff@elkner.net>
2017-02-24 11:22:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# flask scheduler 
+
+An adapted version of the flask scheduler as developed in Ron DuPlain's 
+[Instant Flask Web Development](https://www.packtpub.com/web-development/instant-flask-web-development-instant).
---
+++ sched/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, Flask!'
+
+
+@app.route('/appointments/')
+def appointment_list():
+    return 'Listing of all appointments we have.'
+
+
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+    return 'Detail of appointment #{}.'.format(appointment_id)
+
+
+@app.route('/appointments/<int:appointment_id>/edit/', methods=['GET', 'POST'])
+def appointment_edit(appointment_id):
+    return 'Form to edit appointment #{}.'.format(appointment_id)
+
+
+@app.route('/appointments/create/', methods=['GET', 'POST'])
+def appointment_create():
+    return 'Form to create a new appointment.'
+
+
+@app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+def appointment_delete(appointment_id):
+    raise NotImplementedError('DELETE')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8080)
